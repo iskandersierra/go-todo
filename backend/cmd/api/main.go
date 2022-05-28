af5cc2ba0c7d602c551ea03c62a97403da9afbcc
Iskander <iskander.sierra@gmail.com>
2022-05-28 12:19:24 +0200

    Add echo server and air runner

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	echo "github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	app := echo.New()
+
+    app.GET("/", func(ctx echo.Context) error {
+        return ctx.String(http.StatusOK, "Hello, World!!!")
+    })
+
+    err := app.Start(":8080")
+    app.Logger.Fatal(err)
 }
